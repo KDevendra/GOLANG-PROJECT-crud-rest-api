d688c3277f05beaeecfb6d6fe3a1de6c79b715d9
Devendra verma <88208166+KDevendra@users.noreply.github.com>
2024-05-07 21:43:42 +0530

    update database connection file

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"GOLANG-PROJECT-crud-rest-api/database"
-	"fmt"
+	"database/sql"
+	"log"
+	"os"
 )
 
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
-	database.Connect()
-	fmt.Println("Hello")
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error Database Connection %v", err)
+	}
+	defer db.Close()
 }
